210344988e31d5c8d8d53ec71b91727ebae91bd5
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 23:36:48 -0300

    test: <Button /> should has an onClick with handleClick function

--- src/__tests__/components/Button.test.js
+++ src/__tests__/components/Button.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Button from '~/components/Button';
+
+const buttonData = {
+	label: 'test label',
+	handleClick: jest.fn(),
+};
 
 describe('<Button />', () => {
 	it('should be a function', () => {
@@ -10,10 +15,20 @@ describe('<Button />', () => {
 	});
 
 	it('should has a label', () => {
-		const { getByTestId } = render(<Button label="test label" />);
+		const { getByTestId } = render(<Button {...buttonData} />);
 
 		const button = getByTestId('button');
 
 		expect(button).toHaveTextContent('test label');
+	});
+
+	it('should has an onClick with handleClick function', () => {
+		const { getByTestId } = render(<Button {...buttonData} />);
+
+		const button = getByTestId('button');
+
+		fireEvent.click(button);
+
+		expect(buttonData.handleClick).toBeCalledTimes(1);
 	});
 });
