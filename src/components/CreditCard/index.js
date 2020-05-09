f3d182c8da3f1934251107c1d52299930542aa08
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 22:10:06 -0300

    feat: <CreditCard /> should has a user name element

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 const CreditCard = () => (
 	<div data-testid="credit-card">
 		<span data-testid="credit-card-number" />
+		<span data-testid="credit-name-number" />
 	</div>
 );
 
