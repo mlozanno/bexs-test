27a29909e8fc40079a608864ef87a17a87993d76
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 22:24:08 -0300

    feat: <CreditCard /> should be able to display cardNumber data

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CreditCard = () => (
+const CreditCard = ({ cardNumber }) => (
 	<div data-testid="credit-card">
-		<span data-testid="credit-card-number" />
+		<span data-testid="credit-card-number">{cardNumber}</span>
 		<span data-testid="credit-name-number" />
 		<span data-testid="credit-expiration-number" />
 		<span data-testid="credit-cvv-number" />
 	</div>
 );
+
+CreditCard.propTypes = {
+	cardNumber: PropTypes.string,
+};
+
+CreditCard.defaultProps = {
+	cardNumber: undefined,
+};
 
 export default CreditCard;
