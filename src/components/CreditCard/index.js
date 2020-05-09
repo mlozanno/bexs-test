9c13224484345642179bb40791ff8ea294a43c30
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 22:36:49 -0300

    feat: <CreditCard /> should be able to display expirationDate data

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CreditCard = ({ cardNumber, userName }) => (
+const CreditCard = ({ cardNumber, userName, expirationDate }) => (
 	<div data-testid="credit-card">
 		<span data-testid="credit-card-number">{cardNumber}</span>
 		<span data-testid="credit-card-user-name">{userName}</span>
-		<span data-testid="credit-card-expiration-date" />
+		<span data-testid="credit-card-expiration-date">{expirationDate}</span>
 		<span data-testid="credit-card-cvv" />
 	</div>
 );
@@ -13,11 +13,13 @@ const CreditCard = ({ cardNumber, userName }) => (
 CreditCard.propTypes = {
 	cardNumber: PropTypes.string,
 	userName: PropTypes.string,
+	expirationDate: PropTypes.string,
 };
 
 CreditCard.defaultProps = {
 	cardNumber: undefined,
 	userName: undefined,
+	expirationDate: undefined,
 };
 
 export default CreditCard;
