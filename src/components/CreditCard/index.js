160bff6793414a6da2ff9cfdd40d6a4d51233b80
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 22:41:41 -0300

    feat: <CreditCard /> should be able to display cvv data

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CreditCard = ({ cardNumber, userName, expirationDate }) => (
+const CreditCard = ({ cardNumber, userName, expirationDate, cvv }) => (
 	<div data-testid="credit-card">
 		<span data-testid="credit-card-number">{cardNumber}</span>
 		<span data-testid="credit-card-user-name">{userName}</span>
 		<span data-testid="credit-card-expiration-date">{expirationDate}</span>
-		<span data-testid="credit-card-cvv" />
+		<span data-testid="credit-card-cvv">{cvv}</span>
 	</div>
 );
 
@@ -14,12 +14,14 @@ CreditCard.propTypes = {
 	cardNumber: PropTypes.string,
 	userName: PropTypes.string,
 	expirationDate: PropTypes.string,
+	cvv: PropTypes.string,
 };
 
 CreditCard.defaultProps = {
 	cardNumber: undefined,
 	userName: undefined,
 	expirationDate: undefined,
+	cvv: undefined,
 };
 
 export default CreditCard;
