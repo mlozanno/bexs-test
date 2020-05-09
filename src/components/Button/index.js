bd40d0aa5ce190bb7921348e1fc42b530f5c02f4
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 23:20:32 -0300

    feat: <Button /> should has a label

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,14 @@
-const Button = () => {}
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default Button
+const Button = ({ label }) => (
+	<button type="button" data-testid="button">
+		{label}
+	</button>
+);
+
+Button.propTypes = {
+	label: PropTypes.string.isRequired,
+};
+
+export default Button;
