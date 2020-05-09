da790c99d46f349d4c204887dea8880b4e8b2418
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 21:08:13 -0300

    feat: <CreditCard /> should be a function

---
+++ src/__tests__/components/CreditCard.test.js
@@ -0,0 +1,7 @@
+import CreditCard from '~/components/CreditCard';
+
+describe('<CreditCard />', () => {
+	it('should be a function', () => {
+		expect(typeof CreditCard).toBe('function');
+	});
+});
