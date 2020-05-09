e3c6da5f18e9058cb7144f5b23bbbe5e8fa3f8bf
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 22:31:36 -0300

    feat: <CreditCard /> should be able to display userName data
    fix: data-testid names

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CreditCard = ({ cardNumber }) => (
+const CreditCard = ({ cardNumber, userName }) => (
 	<div data-testid="credit-card">
 		<span data-testid="credit-card-number">{cardNumber}</span>
-		<span data-testid="credit-name-number" />
-		<span data-testid="credit-expiration-number" />
-		<span data-testid="credit-cvv-number" />
+		<span data-testid="credit-card-user-name">{userName}</span>
+		<span data-testid="credit-card-expiration-date" />
+		<span data-testid="credit-card-cvv" />
 	</div>
 );
 
 CreditCard.propTypes = {
 	cardNumber: PropTypes.string,
+	userName: PropTypes.string,
 };
 
 CreditCard.defaultProps = {
 	cardNumber: undefined,
+	userName: undefined,
 };
 
 export default CreditCard;
