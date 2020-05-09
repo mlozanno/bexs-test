01dd2a0662e04fbb44bee914b89369469e7e2227
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 22:13:09 -0300

    feat: <CreditCard /> should has an expiration date element

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -4,6 +4,7 @@ const CreditCard = () => (
 	<div data-testid="credit-card">
 		<span data-testid="credit-card-number" />
 		<span data-testid="credit-name-number" />
+		<span data-testid="credit-expiration-number" />
 	</div>
 );
 
