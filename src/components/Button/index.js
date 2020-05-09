ba061d3259145668ed05c91c755ed39f8b19f498
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 23:36:42 -0300

    feat: <Button /> should has an onClick with handleClick function

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ label }) => (
-	<button type="button" data-testid="button">
+const Button = ({ label, handleClick }) => (
+	<button type="button" data-testid="button" onClick={handleClick}>
 		{label}
 	</button>
 );
 
 Button.propTypes = {
 	label: PropTypes.string.isRequired,
+	handleClick: PropTypes.func.isRequired,
 };
 
 export default Button;
