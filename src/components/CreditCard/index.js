0394856873306c8379193a309f718101cb6e16e1
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 22:58:18 -0300

    feat: <CreditCard /> should be able to display "NOME DO TITULAR" if userName is missing

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const CreditCard = ({ cardNumber, userName, expirationDate, cvv }) => (
 	<div data-testid="credit-card">
 		<span data-testid="credit-card-number">{cardNumber}</span>
-		<span data-testid="credit-card-user-name">{userName}</span>
+		<span data-testid="credit-card-user-name">{userName.toUpperCase()}</span>
 		<span data-testid="credit-card-expiration-date">{expirationDate}</span>
 		<span data-testid="credit-card-cvv">{cvv}</span>
 	</div>
@@ -19,7 +19,7 @@ CreditCard.propTypes = {
 
 CreditCard.defaultProps = {
 	cardNumber: '**** **** **** ****',
-	userName: undefined,
+	userName: 'nome do titular',
 	expirationDate: undefined,
 	cvv: undefined,
 };
