20d151f13c97c157817f9761e4e332e03b69813e
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 23:20:39 -0300

    test: <Button /> should has a label

--- src/__tests__/components/Button.test.js
+++ src/__tests__/components/Button.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Button from '~/components/Button';
@@ -7,5 +7,13 @@ import Button from '~/components/Button';
 describe('<Button />', () => {
 	it('should be a function', () => {
 		expect(typeof Button).toBe('function');
+	});
+
+	it('should has a label', () => {
+		const { getByTestId } = render(<Button label="test label" />);
+
+		const button = getByTestId('button');
+
+		expect(button).toHaveTextContent('test label');
 	});
 });
