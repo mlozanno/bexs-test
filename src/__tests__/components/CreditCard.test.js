c26327b05fa65677eab10e2477b44067aca51f4c
Marcus Lozano <contato@lozanno.com.br>
2020-05-08 21:55:53 -0300

    test: <CreditCard /> should has a card number element

--- src/__tests__/components/CreditCard.test.js
+++ src/__tests__/components/CreditCard.test.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
 import CreditCard from '~/components/CreditCard';
 
 describe('<CreditCard />', () => {
 	it('should be a function', () => {
 		expect(typeof CreditCard).toBe('function');
+	});
+
+	it('should has a card number element', () => {
+		const { getByTestId } = render(<CreditCard />);
+
+		const cardNumber = getByTestId('credit-card-number');
+
+		expect(cardNumber).toBeDefined();
 	});
 });
