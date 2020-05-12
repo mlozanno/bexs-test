cf9c81cfe1f687fe52e55b09e137225eefc0a1b4
Marcus Lozano <contato@lozanno.com.br>
2020-05-12 20:48:17 -0300

    feat: add snapshot test

--- src/__tests__/components/Nav.test.js
+++ src/__tests__/components/Nav.test.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import Nav from '~/components/Nav';
 
 describe('<Nav />', () => {
 	it('should be a function', () => {
 		expect(typeof Nav).toBe('function');
+	});
+
+	it('should be render component', () => {
+		const { getByTestId } = render(<Nav />);
+
+		const nav = getByTestId('nav');
+
+		expect(nav).toMatchSnapshot();
 	});
 });
