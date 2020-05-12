6edc3b878fe2d359dd000668b1d9d9b866e16d6f
Marcus Lozano <contato@lozanno.com.br>
2020-05-12 20:47:44 -0300

    reafactory: code organization

--- src/__tests__/components/CreditCard.test.js
+++ src/__tests__/components/CreditCard.test.js
@@ -6,7 +6,7 @@ const cardData = {
 	cardNumber: '123123123',
 	userName: 'MARCUS LOZANO',
 	expirationDate: '10/23',
-	cvv: '123'
+	cvv: '123',
 };
 
 describe('<CreditCard />', () => {
@@ -14,99 +14,71 @@ describe('<CreditCard />', () => {
 		expect(typeof CreditCard).toBe('function');
 	});
 
-	it('should has a card number element', () => {
-		const { getByTestId } = render(<CreditCard />);
+	describe('Real data', () => {
+		it('should be able to display cardNumber data', () => {
+			const { getByTestId } = render(<CreditCard {...cardData} />);
 
-		const cardNumber = getByTestId('credit-card-number');
+			const cardNumber = getByTestId('credit-card-number');
 
-		expect(cardNumber).toBeDefined();
+			expect(cardNumber).toHaveTextContent(cardData.cardNumber);
+		});
+
+		it('should be able to display userName data', () => {
+			const { getByTestId } = render(<CreditCard {...cardData} />);
+
+			const userName = getByTestId('credit-card-user-name');
+
+			expect(userName).toHaveTextContent(cardData.userName);
+		});
+
+		it('should be able to display expirationDate data', () => {
+			const { getByTestId } = render(<CreditCard {...cardData} />);
+
+			const expirationDate = getByTestId('credit-card-expiration-date');
+
+			expect(expirationDate).toHaveTextContent(cardData.expirationDate);
+		});
+
+		it('should be able to display cvv data', () => {
+			const { getByTestId } = render(<CreditCard {...cardData} />);
+
+			const cvv = getByTestId('credit-card-cvv');
+
+			expect(cvv).toHaveTextContent(cardData.cvv);
+		});
 	});
 
-	it('should has an user name element', () => {
-		const { getByTestId } = render(<CreditCard />);
+	describe('Plaholder data', () => {
+		it('should be able to display "**** **** **** ****" if cardNumber is missing', () => {
+			const { getByTestId } = render(<CreditCard />);
 
-		const userName = getByTestId('credit-card-user-name');
+			const cardNumber = getByTestId('credit-card-number');
 
-		expect(userName).toBeDefined();
-	});
+			expect(cardNumber).toHaveTextContent('**** **** **** ****');
+		});
 
-	it('should has an expiration date element', () => {
-		const { getByTestId } = render(<CreditCard />);
+		it('should be able to display "NOME DO TITULAR" if userName is missing', () => {
+			const { getByTestId } = render(<CreditCard />);
 
-		const expirationDate = getByTestId('credit-card-expiration-date');
+			const userName = getByTestId('credit-card-user-name');
 
-		expect(expirationDate).toBeDefined();
-	});
+			expect(userName).toHaveTextContent('NOME DO TITULAR');
+		});
 
-	it('should has a cvv element', () => {
-		const { getByTestId } = render(<CreditCard />);
+		it('should be able to display "00/00" if expirationDate is missing', () => {
+			const { getByTestId } = render(<CreditCard />);
 
-		const cvv = getByTestId('credit-card-cvv');
+			const userName = getByTestId('credit-card-expiration-date');
 
-		expect(cvv).toBeDefined();
-	});
+			expect(userName).toHaveTextContent('00/00');
+		});
 
-	it('should be able to display cardNumber data', () => {
-		const { getByTestId } = render(<CreditCard {...cardData} />);
+		it('should be able to display "***" if cvv is missing', () => {
+			const { getByTestId } = render(<CreditCard />);
 
-		const cardNumber = getByTestId('credit-card-number');
+			const userName = getByTestId('credit-card-cvv');
 
-		expect(cardNumber).toHaveTextContent(cardData.cardNumber);
-	});
-
-	it('should be able to display userName data', () => {
-		const { getByTestId } = render(<CreditCard {...cardData} />);
-
-		const userName = getByTestId('credit-card-user-name');
-
-		expect(userName).toHaveTextContent(cardData.userName);
-	});
-
-	it('should be able to display expirationDate data', () => {
-		const { getByTestId } = render(<CreditCard {...cardData} />);
-
-		const expirationDate = getByTestId('credit-card-expiration-date');
-
-		expect(expirationDate).toHaveTextContent(cardData.expirationDate);
-	});
-
-	it('should be able to display cvv data', () => {
-		const { getByTestId } = render(<CreditCard {...cardData} />);
-
-		const cvv = getByTestId('credit-card-cvv');
-
-		expect(cvv).toHaveTextContent(cardData.cvv);
-	});
-
-	it('should be able to display "**** **** **** ****" if cardNumber is missing', () => {
-		const { getByTestId } = render(<CreditCard />);
-
-		const cardNumber = getByTestId('credit-card-number');
-
-		expect(cardNumber).toHaveTextContent('**** **** **** ****');
-	});
-
-	it('should be able to display "NOME DO TITULAR" if userName is missing', () => {
-		const { getByTestId } = render(<CreditCard />);
-
-		const userName = getByTestId('credit-card-user-name');
-
-		expect(userName).toHaveTextContent('NOME DO TITULAR');
-	});
-
-	it('should be able to display "00/00" if expirationDate is missing', () => {
-		const { getByTestId } = render(<CreditCard />);
-
-		const userName = getByTestId('credit-card-expiration-date');
-
-		expect(userName).toHaveTextContent('00/00');
-	});
-
-	it('should be able to display "***" if cvv is missing', () => {
-		const { getByTestId } = render(<CreditCard />);
-
-		const userName = getByTestId('credit-card-cvv');
-
-		expect(userName).toHaveTextContent('***');
+			expect(userName).toHaveTextContent('***');
+		});
 	});
 });
