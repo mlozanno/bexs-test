a6e3c23dfa31cb7a37db05962632cc2e213e41e8
Marcus Lozano <contato@lozanno.com.br>
2020-05-12 20:48:27 -0300

    feat: create nav structure

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,3 +1,19 @@
-const Nav = () => {};
+import React from 'react';
+
+const Nav = () => (
+	<ul data-testid="nav">
+		<li>
+			<a href="/">Home</a>
+		</li>
+
+		<li>
+			<a href="/carteira">Carteira</a>
+		</li>
+
+		<li>
+			<a href="/contato">Contato</a>
+		</li>
+	</ul>
+);
 
 export default Nav;
