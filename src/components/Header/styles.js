4a9efb713d5229fd0151e973b2a242a9b805e4b2
Marcus Lozano <contato@lozanno.com.br>
2020-05-13 20:10:57 -0300

    feat: add height and border to header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
+import { spacing, colors } from '~/styles/theme';
 
 export const StyledHeader = styled.header`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
+	height: ${spacing(7)};
+	padding: ${spacing(0, 2)};
+	background-color: ${colors.light};
+	border-bottom: 1px solid ${colors.darkGrey};
 
 	img {
 		display: block;
