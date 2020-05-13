e30913afbf0f8eae989307e4f9a5640e00c57b03
Marcus Lozano <contato@lozanno.com.br>
2020-05-12 23:23:10 -0300

    test: create Wallet page

---
+++ src/__tests__/pages/Wallet.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import Wallet from '~/pages/Wallet';
+
+describe('<Wallet /> page', () => {
+	it('should be a function', () => {
+		expect(typeof Wallet).toBe('function');
+	});
+
+	it('should be render component', () => {
+		const { getByTestId } = render(<Wallet />);
+
+		const wallet = getByTestId('wallet-page');
+
+		expect(wallet).toMatchSnapshot();
+	});
+});
