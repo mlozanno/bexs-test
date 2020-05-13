586635b95a20111010a0eead965869cfd1c7e869
Marcus Lozano <contato@lozanno.com.br>
2020-05-12 23:28:02 -0300

    feat: add Link from react-router-dom

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
 	<ul data-testid="nav">
 		<li>
-			<a href="/">Home</a>
+			<Link to="/">Home</Link>
 		</li>
 
 		<li>
-			<a href="/carteira">Carteira</a>
+			<Link to="/carteira">Carteira</Link>
 		</li>
 
 		<li>
-			<a href="/contato">Contato</a>
+			<Link to="/checkout">Checkout</Link>
 		</li>
 	</ul>
 );
