63865766229bede50c4a04a1c8df2e85e2326656
Marcus Lozano <contato@lozanno.com.br>
2020-05-14 18:41:14 -0300

    reafactory: add padding to the layout

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import { spacing } from '~/styles/theme';
 
 export const StyledLayout = styled.section`
 	max-width: 1024px;
 	margin: 0 auto;
+	padding: ${spacing(0, 2)};
 `;
