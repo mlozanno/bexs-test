3eaa725eb1c57ea1aa66319a8b23f9e5cb45c264
Marcus Lozano <contato@lozanno.com.br>
2020-05-14 18:42:04 -0300

    refactory: move mobile menu style to the media >= 768

--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -2,27 +2,34 @@ import styled from 'styled-components';
 import { spacing, zIndex, colors } from '~/styles/theme';
 
 export const StyledNav = styled.nav`
-	position: fixed;
-	top: 0;
-	bottom: 0;
-	left: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 100%;
-	height: 100vh;
-	background-color: ${colors.primary};
+	@media (max-width: 767px) {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100vh;
+		background-color: ${colors.primary};
 
-	a {
-		color: white;
-	}
+		a {
+			color: white;
+		}
 
-	&.opened {
-		left: 0;
+		&.opened {
+			left: 0;
+		}
 	}
 `;
 
 export const ToggleButton = styled.button`
+	margin: 0;
+	padding: 0;
+	background: transparent;
+	border: none;
+
 	&.close {
 		position: absolute;
 		top: ${spacing(2)};
@@ -30,12 +37,26 @@ export const ToggleButton = styled.button`
 		z-index: ${zIndex.floated};
 		color: ${colors.light};
 	}
+
+	@media (min-width: 768px) {
+		display: none;
+	}
 `;
 
 export const MenuList = styled.ul`
-	text-align: center;
+	@media (max-width: 767px) {
+		text-align: center;
 
-	> li + li {
-		margin-top: ${spacing(2)};
+		> li + li {
+			margin-top: ${spacing(2)};
+		}
+	}
+
+	@media (min-width: 768px) {
+		display: flex;
+
+		> li + li {
+			margin-left: ${spacing(2)};
+		}
 	}
 `;
