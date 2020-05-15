868def9857f774c6a5a307c82d4c9222d8c0cffc
Marcus Lozano <contato@lozanno.com.br>
2020-05-14 23:03:25 -0300

    feat: add credit card to checkout page

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Checkout = () => <div data-testid="checkout-page" />;
+import CreditCard from '~/components/CreditCard';
+
+import { StyledCardContainer } from './styles';
+
+const Checkout = () => (
+	<section data-testid="checkout-page">
+		<StyledCardContainer>
+			<CreditCard />
+		</StyledCardContainer>
+	</section>
+);
 
 export default Checkout;
