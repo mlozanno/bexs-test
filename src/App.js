4ab388a08ffb4d0ede409200144143c4566cfa8d
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 06:46:38 -0300

    feat: add card context provider and Material theme provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { CardProvider } from '~/context/card';
 import GlobalStyles from '~/styles/global';
+import { colors } from '~/styles/theme';
 import Layout from '~/components/Layout';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: colors.primary,
+		},
+	},
+});
+
 const App = () => (
-	<>
-		<Layout />
-		<GlobalStyles />
-	</>
+	<CardProvider>
+		<MuiThemeProvider theme={theme}>
+			<Layout />
+			<GlobalStyles />
+		</MuiThemeProvider>
+	</CardProvider>
 );
 
 export default App;
