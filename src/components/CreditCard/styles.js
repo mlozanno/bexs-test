64103b7dc9a0c2fda745d557763c5cc864982b41
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 06:56:23 -0300

    refactory: change 'spacing' theme function to 'toRem' feat: add brand change by state

--- src/components/CreditCard/styles.js
+++ src/components/CreditCard/styles.js
@@ -1,31 +1,43 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { rgba } from 'polished';
-import { spacing, colors } from '~/styles/theme';
+import { toRem, colors } from '~/styles/theme';
 
 import frontInvalid from '~/assets/front-invalid.svg';
 import backInvalid from '~/assets/back-invalid.svg';
 import frontValid from '~/assets/front-valid.svg';
 import backValid from '~/assets/back-valid.svg';
 
+import brandsImg from '~/assets/card-brands/brands.png';
+
+const brandsDimensions = [
+	{ type: 'maestro', width: 50, x: -0 },
+	{ type: 'amex', width: 87, x: -64 },
+	{ type: 'visa', width: 99, x: -164 },
+	{ type: 'mastercard', width: 50, x: -277 },
+	{ type: 'dinersclub', width: 44, x: -341 },
+	{ type: 'jcb', width: 42, x: -398 },
+	{ type: 'discover', width: 42, x: -454 },
+];
+
 export const StyledCreditCard = styled.div`
 	span {
-		font-size: 1.2rem;
+		font-size: ${toRem(1.2)};
 
 		@media (min-width: 768px) {
-			font-size: 1.6rem;
+			font-size: ${toRem(1.6)};
 		}
 	}
 
 	span[data-field='card-number'],
-	span[data-field='card-cvv'] {
-		font-size: 1.9rem;
+	span[data-field='card-cvc'] {
+		font-size: ${toRem(1.9)};
 
 		@media (min-width: 768px) {
-			font-size: 2.2rem;
+			font-size: ${toRem(2.2)};
 		}
 	}
 
-	span[data-field='card-cvv'] {
+	span[data-field='card-cvc'] {
 		position: absolute;
 		color: ${colors.darkGrey};
 		transform: translate(135px, 80px);
@@ -34,16 +46,16 @@ export const StyledCreditCard = styled.div`
 
 export const FliperContainer = styled.div`
 	position: relative;
-	width: 28rem;
-	height: 17.2rem;
-	perspective: 100rem;
+	width: ${toRem(28)};
+	height: ${toRem(17.2)};
+	perspective: ${toRem(100)};
 
 	&::before {
 		position: absolute;
-		bottom: -0.5rem;
+		bottom: ${toRem(-0.5)};
 		left: 50%;
 		width: 90%;
-		height: 0.5rem;
+		height: ${toRem(0.5)};
 		background-color: ${rgba(colors.darkGrey, 0.3)};
 		border-radius: 50%;
 		transform: translateX(-50%);
@@ -52,8 +64,8 @@ export const FliperContainer = styled.div`
 	}
 
 	@media (min-width: 768px) {
-		width: 36.4rem;
-		height: 22.3rem;
+		width: ${toRem(36.4)};
+		height: ${toRem(22.3)};
 	}
 `;
 
@@ -61,7 +73,7 @@ export const Flipper = styled.div`
 	position: relative;
 	width: 100%;
 	height: 100%;
-	border-radius: 1rem;
+	border-radius: ${toRem(1)};
 	transform-style: preserve-3d;
 	transition: transform 0.3s ease-in-out;
 
@@ -77,7 +89,7 @@ export const Front = styled.div`
 	justify-content: space-between;
 	width: 100%;
 	height: 100%;
-	padding: ${spacing(3, 2)};
+	padding: ${toRem(3, 2)};
 	color: ${colors.light};
 	text-shadow: 0px 1px 2px #000000b3;
 	background: url(${({ valid }) => (!valid ? frontInvalid : frontValid)});
@@ -89,11 +101,11 @@ export const Front = styled.div`
 
 	span[data-field='card-number'] {
 		grid-column: span 4;
-		letter-spacing: 0.14rem;
+		letter-spacing: ${toRem(0.14)};
 		white-space: nowrap;
 
 		@media (min-width: 768px) {
-			letter-spacing: 0.4rem;
+			letter-spacing: ${toRem(0.4)};
 		}
 	}
 
@@ -101,17 +113,30 @@ export const Front = styled.div`
 		grid-column: span 3;
 	}
 
-	span[data-field='card-expiration-date'] {
+	span[data-field='card-expiry-date'] {
 		grid-column: span 1;
 		text-align: right;
 	}
 
 	&::before {
 		grid-column: span 4;
-		width: 70px;
-		height: 30px;
-		background-color: #f90;
+		width: 0;
+		height: ${toRem(3)};
+		background: url(${brandsImg}) no-repeat;
 		content: '';
+
+		${({ brand, valid }) => {
+			if (brand) {
+				const brandObj = brandsDimensions.find(item => item.type === brand);
+
+				return css`
+					width: ${toRem(!valid ? 0 : brandObj.width / 10)};
+					background-position-x: ${toRem(brandObj.x / 10)};
+				`;
+			}
+
+			return false;
+		}};
 	}
 `;
 
