57eefbff11b18c74907711e32d27178949a0b2f7
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 09:42:37 -0300

    test: update checkout page test with context state

--- src/__tests__/pages/Checkout.test.js
+++ src/__tests__/pages/Checkout.test.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
+import { CardProvider } from '~/context/card';
+
 import Checkout from '~/pages/Checkout';
 
-describe('<Checkout /> page', () => {
-	it('should be a function', () => {
-		expect(typeof Checkout).toBe('function');
+describe(`<Checkout /> page`, () => {
+	it(`should be a function`, () => {
+		expect(typeof Checkout).toBe(`function`);
 	});
 
-	it('should be render component', () => {
-		const { getByTestId } = render(<Checkout />);
+	it(`should be render component`, () => {
+		const { getByTestId } = render(
+			<CardProvider>
+				<MemoryRouter>
+					<Checkout />
+				</MemoryRouter>
+			</CardProvider>
+		);
 
-		const checkout = getByTestId('checkout-page');
+		const checkout = getByTestId(`checkout-page`);
 
 		expect(checkout).toMatchSnapshot();
 	});
