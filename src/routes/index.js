89a8b136d890ae7cdd34f874cf81ac7082b6df5c
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 09:59:43 -0300

    refactory: add redirect `/` => `/checkout`;

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Shop from '~/pages/Shop';
 import Checkout from '~/pages/Checkout';
@@ -8,7 +8,8 @@ import Wallet from '~/pages/Wallet';
 
 const Routes = () => (
 	<Switch>
-		<Route path="/" exact component={Shop} />
+		<Route exact path="/" render={() => <Redirect to="/checkout" />} />
+		<Route path="/shop" exact component={Shop} />
 		<Route path="/checkout" exact component={Checkout} />
 		<Route path="/carteira" exact component={Wallet} />
 	</Switch>
