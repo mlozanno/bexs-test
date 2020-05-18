740949b722637c087f52deaf2f093cd0edc0e6bf
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 06:40:45 -0300

    feat: update theme file

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,24 +1,27 @@
 export const base = 10;
 
 export const colors = {
-	primary: '#DE4B4B',
 	darkGrey: '#3C3C3C',
-	lightGrey: '#F7F7F7',
+	error: '#EB5757',
+	grey: '#c9c9c9',
 	light: '#fff',
+	lightGrey: '#F7F7F7',
+	primary: '#DE4B4B',
 };
 
 export const fontSize = {
 	large: `${22 / base}rem`, // 2.2rem
-	regular: `${17 / base}rem`, // 1.6rem
+	regular: `${17 / base}rem`, // 1.7rem
 	small: `${13 / base}rem`, // 1.3rem
 };
 
 export const zIndex = {
 	base: 0,
 	floated: base,
+	super: 100,
 };
 
-export const spacing = (...values) =>
+export const toRem = (...values) =>
 	values
 		.map(value => (Number(value) ? `${(value * base) / base}rem` : value))
 		.join(' ');
