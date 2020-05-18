d0c1031d2aecd5a4509f784cd219bd8b9225e7d8
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 06:51:04 -0300

    fix: remove padding controll from layout

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
-import { spacing } from '~/styles/theme';
 
 export const StyledLayout = styled.section`
 	max-width: 1024px;
 	margin: 0 auto;
-	padding: ${spacing(0, 2)};
 `;
