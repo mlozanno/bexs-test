6431629596f4d98e3e489766ed68269f5e8249c6
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 09:42:14 -0300

    feat: update layout test with context state

--- src/__tests__/components/Layout.test.js
+++ src/__tests__/components/Layout.test.js
@@ -3,79 +3,91 @@ import { MemoryRouter } from 'react-router';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
+import { CardProvider } from '~/context/card';
+
 import Layout from '~/components/Layout';
 
-describe('<Layout />', () => {
-	it('should be a function', () => {
-		expect(typeof Layout).toBe('function');
+describe(`<Layout />`, () => {
+	it(`should be a function`, () => {
+		expect(typeof Layout).toBe(`function`);
 	});
 
-	it('should be render component', () => {
+	it(`should be render component`, () => {
 		const { getByTestId } = render(
-			<MemoryRouter>
-				<Layout />
-			</MemoryRouter>
+			<CardProvider>
+				<MemoryRouter>
+					<Layout />
+				</MemoryRouter>
+			</CardProvider>
 		);
 
-		const layout = getByTestId('layout');
+		const layout = getByTestId(`layout`);
 
 		expect(layout).toMatchSnapshot();
 	});
 
-	it('should be able to render Shop component when click in logo', () => {
+	it(`should be able to render Shop component when click in logo`, () => {
 		const { getByTestId, getByAltText, getByText } = render(
-			<MemoryRouter>
-				<Layout />
-			</MemoryRouter>
+			<CardProvider>
+				<MemoryRouter>
+					<Layout />
+				</MemoryRouter>
+			</CardProvider>
 		);
 
 		fireEvent.click(getByText(/Checkout/i));
 
-		fireEvent.click(getByAltText('Bexs logo'));
+		fireEvent.click(getByAltText(`Bexs logo`));
 
-		const shopPage = getByTestId('shop-page');
+		const shopPage = getByTestId(`shop-page`);
 
 		expect(shopPage).toBeTruthy();
 	});
 
-	it('should be able to render Shop component when click in Home link', () => {
+	it(`should be able to render Shop component when click in Home link`, () => {
 		const { getByTestId, getByText } = render(
-			<MemoryRouter>
-				<Layout />
-			</MemoryRouter>
+			<CardProvider>
+				<MemoryRouter>
+					<Layout />
+				</MemoryRouter>
+			</CardProvider>
 		);
 
 		fireEvent.click(getByText(/Home/i));
 
-		const shopPage = getByTestId('shop-page');
+		const shopPage = getByTestId(`shop-page`);
 
 		expect(shopPage).toBeTruthy();
 	});
 
-	it('should be able to render Checkout component when click in Checkout link', () => {
+	it(`should be able to render Checkout component when click in Checkout link`, () => {
 		const { getByTestId, getByText } = render(
-			<MemoryRouter>
-				<Layout />
-			</MemoryRouter>
+			<CardProvider>
+				<MemoryRouter>
+					<Layout />
+				</MemoryRouter>
+			</CardProvider>
 		);
 
 		fireEvent.click(getByText(/Checkout/i));
 
-		const checkoutPage = getByTestId('checkout-page');
+		const checkoutPage = getByTestId(`checkout-page`);
 
 		expect(checkoutPage).toBeTruthy();
 	});
 
-	it('should be able to render Wallet component when click in Carteira link', () => {
+	it(`should be able to render Wallet component when click in Carteira link`, () => {
 		const { getByTestId, getByText } = render(
-			<MemoryRouter>
-				<Layout />
-			</MemoryRouter>
+			<CardProvider>
+				<MemoryRouter>
+					<Layout />
+				</MemoryRouter>
+			</CardProvider>
 		);
 
 		fireEvent.click(getByText(/Carteira/i));
 
-		const walletPage = getByTestId('wallet-page');
+		const walletPage = getByTestId(`wallet-page`);
 
 		expect(walletPage).toBeTruthy();
 	});
