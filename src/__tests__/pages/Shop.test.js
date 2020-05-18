440ecabeb7323ab540fb386dd32b1e4c23ce1b06
Marcus Lozano <contato@lozanno.com.br>
2020-05-18 09:07:01 -0300

    lint: singleQuote => backtick

--- src/__tests__/pages/Shop.test.js
+++ src/__tests__/pages/Shop.test.js
@@ -4,15 +4,15 @@ import '@testing-library/jest-dom';
 
 import Shop from '~/pages/Shop';
 
-describe('<Shop /> page', () => {
-	it('should be a function', () => {
-		expect(typeof Shop).toBe('function');
+describe(`<Shop /> page`, () => {
+	it(`should be a function`, () => {
+		expect(typeof Shop).toBe(`function`);
 	});
 
-	it('should be render component', () => {
+	it(`should be render component`, () => {
 		const { getByTestId } = render(<Shop />);
 
-		const shop = getByTestId('shop-page');
+		const shop = getByTestId(`shop-page`);
 
 		expect(shop).toMatchSnapshot();
 	});
